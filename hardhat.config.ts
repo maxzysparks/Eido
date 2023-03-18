c7590a86f7a356450ecc4284034b4ea4c62adc98
Maxwell <maxwellonyeka2487@gmail.com>
2023-03-18 20:26:24 +0100

    eido

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+};
+
+export default config;
+
+module.exports = {
+  // ...
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+};
